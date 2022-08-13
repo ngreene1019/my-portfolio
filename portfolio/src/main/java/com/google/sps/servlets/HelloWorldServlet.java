6a2b4cf51963df0ac98f9827056cfdc9728e4b76
Najah Greene <ngreene1019@gmail.com>
2022-08-13 04:41:08 +0000

    add incomplete returned list of messages

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,37 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/thankYou")
+@WebServlet("/myHobbies")
 public class HelloWorldServlet extends HttpServlet {
+    
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    ArrayList<String> hobbies = new ArrayList<String>();
+    hobbies.add("Baking");
+    hobbies.add("Reading");
+    hobbies.add("Litsening to music");
+    hobbies.add("Spending Time Outdoors");
 
+
+   
+  }
+}
+
+/**@WebServlet("/thankYou")
+public class HelloWorldServlet extends HttpServlet {
+    
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("Thank you for visiting!");
   }
-}
+}**/
+
