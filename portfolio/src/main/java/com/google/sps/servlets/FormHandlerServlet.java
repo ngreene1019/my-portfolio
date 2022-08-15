ab7b4e510f55007afc932bcf2de7513ad18d95e4
Najah Greene <ngreene1019@gmail.com>
2022-08-15 12:54:45 +0000

    add datastore

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -6,6 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+
 @WebServlet("/contact-form")
 public class FormHandlerServlet extends HttpServlet {
 
@@ -16,7 +22,35 @@ public class FormHandlerServlet extends HttpServlet {
     String nameValue = request.getParameter("name-input");
     String emailValue = request.getParameter("email-input");
     String messageValue = request.getParameter("message-input");
+    
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Info");
+    FullEntity infoEntity =
+    Entity.newBuilder(keyFactory.newKey())
+        .set("nameValue", nameValue)
+        .set("emailValue", emailValue)
+        .set("messageValue", messageValue)
+        .build();
 
+        datastore.put(infoEntity);
+
+
+    //Redirect to home page
+    response.sendRedirect("https://ngreene-sps-summer22.appspot.com/");
+  }
+}
+
+/*@WebServlet("/contact-form")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String nameValue = request.getParameter("name-input");
+    String emailValue = request.getParameter("email-input");
+    String messageValue = request.getParameter("message-input");
+    
     // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + nameValue);
     System.out.println("You submitted: " + emailValue);
@@ -30,4 +64,4 @@ public class FormHandlerServlet extends HttpServlet {
     //Redirect to home page
     response.sendRedirect("https://ngreene-sps-summer22.appspot.com/");
   }
-}
+}*/
